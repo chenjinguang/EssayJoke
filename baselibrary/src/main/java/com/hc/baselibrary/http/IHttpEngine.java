6db1d99fc2116128e1e6a522a66bf91dc250641b
240336124 <240336124@qq.com>
2017-03-13 20:42:06 +0800

    first commit

---
+++ baselibrary/src/main/java/com/hc/baselibrary/http/IHttpEngine.java
@@ -0,0 +1,10 @@
+package com.hc.baselibrary.http;
+
+/**
+ * Email 240336124@qq.com
+ * Created by Darren on 2017/3/4.
+ * Version 1.0
+ * Description:
+ */
+public class IHttpEngine {
+}
---
+++ framelibrary/src/main/java/com/example/administrator/framelibrary/db/DaoSupportFactory.java
@@ -0,0 +1,10 @@
+package com.example.administrator.framelibrary.db;
+
+/**
+ * Email 240336124@qq.com
+ * Created by Darren on 2017/3/5.
+ * Version 1.0
+ * Description:
+ */
+public class DaoSupportFactory {
+}
